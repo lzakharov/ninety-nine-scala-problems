c7dd8f24ee966db938773acf9e9df228ec0eb7fb
Lev Zakharov <l.j.zakharov@gmail.com>
2019-05-08 13:59:04 +0300

    Remove unimlemented test spec

--- src/test/scala/ru/lzakharov/nintynineproblems/arithmetic/ArithmeticSpec.scala
+++
@@ -1,9 +0,0 @@
-package ru.lzakharov.nintynineproblems.arithmetic
-
-import ru.lzakharov.nintynineproblems.logic.P46._
-
-object ArithmeticSpec {
-  def main(args: Array[String]): Unit = {
-    table2(and)
-  }
-}
